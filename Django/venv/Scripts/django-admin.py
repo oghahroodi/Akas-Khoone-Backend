38ecda09f1c3dd2e8a54fa901181c7a68abe27ab
sepehr arabnoori <sepehr2012a@gmail.com>
2018-08-19 11:50:44 +0430

    delete venv

--- Django/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\rahnema\Mainproject\Akas_khoone_back\Django\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
