3b240baafddf85e1b99273976ea3b948cec02896
Omid Ghahroudi <omid.username.goes.here@gmail.com>
2018-08-19 13:20:50 +0430

    add postmodel #TFOUR13

--- Django/Akas_khoone/Core/admin.py
+++ Django/Akas_khoone/Core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Pic
+from .postModels import Post, PicPost
 
 admin.site.register(Person)
+admin.site.register(Post)
+admin.site.register(Pic)
+admin.site.register(PicPost)
+
 
