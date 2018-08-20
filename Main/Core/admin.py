ab25c8778c7b0120995932fcafff1d2cd134d288
sepehr arabnoori <sepehr2012a@gmail.com>
2018-08-20 09:33:28 +0430

    merge staged

--- Main/Core/admin.py
+++ Main/Core/admin.py
@@ -1,21 +1,11 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .postModels import Post, PicPost
-from .personModels import Person,PicPerson
-
-admin.site.register(Post)
-admin.site.register(PicPost)
-admin.site.register(PicPerson)
-admin.site.register(Person)
-# Register your models here.
-=======
 from .postModels import Post, Tag, TagPost, PicPost
-from .personModels import Person, Pic
+from .personModels import Person, PicPerson
 
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(TagPost)
 admin.site.register(PicPost)
 admin.site.register(Person)
-admin.site.register(Pic)
->>>>>>> 0392953419b3dea0b84147d9bd65b2bca02c007d
+admin.site.register(PicPerson)
+
--- Main/Core/postSerilizers.py
+++ Main/Core/postSerilizers.py
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
+
 from rest_framework import serializers
 
 from .postModels import Post
 
 from django.contrib.auth import authenticate
 
-
+#create post serilizers
 class PostSerilizer(serializers.Serializer):
     description = serializers.CharField(max_length=250, null=True)
     likeNumber = serializers.IntegerField(default=0)
@@ -17,6 +17,3 @@ class PostSerilizer(serializers.Serializer):
         model = Post
         fields = ('id', 'description', 'likeNumber', 'commentNumber', 'date',)
 
-=======
-#create post serilizers
->>>>>>> 0392953419b3dea0b84147d9bd65b2bca02c007d
