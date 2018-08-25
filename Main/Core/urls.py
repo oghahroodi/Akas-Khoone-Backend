661fa101acfc3989dbaa7ead9a5c7d70ba65f64b
sepehr arabnoori <sepehr2012a@gmail.com>
2018-08-25 08:42:42 +0430

    validation and change of athurization

--- Main/Core/urls.py
+++ Main/Core/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from rest_framework_simplejwt.views import TokenObtainPairView , TokenRefreshView
 from . import views
 urlpatterns = [
-path('login/',  obtain_jwt_token, name='login'),
-path('login/refresh/',  refresh_jwt_token, name='refresh'),
+path('login/',  TokenObtainPairView.as_view(), name='login'),
+path('login/refresh/',  TokenRefreshView.as_view(), name='refresh'),
 path('profile/info/', views.sendInfo.as_view()),
 ]
