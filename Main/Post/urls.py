a4cf708a29dc32b703245db8ca74df72e5fe0370
sepehr arabnoori <sepehr2012a@gmail.com>
2018-09-03 06:40:07 +0430

    homeposts

--- Main/Post/urls.py
+++ Main/Post/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('profile/posts/', views.ProfilePosts.as_view()),
-    path('post/<int:pk>/', views.PostDetails.as_view())
-    ]
+    path('post/<int:pk>/', views.PostDetails.as_view()),
+    path('home/posts/', views.HomePosts.as_view()),
+]
--- Main/Post/views.py
+++ Main/Post/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from .serializers import *
+from Account.models import *
 from django.http import JsonResponse
 from rest_framework import generics
 from rest_framework.pagination import *
@@ -9,12 +10,10 @@ from .utilities import extractHashtags
 
 
 class PostDetails(APIView):
-
     def get(self, request, pk):
         post = Post.objects.get(id=pk)
         serializer = PostSerializer(post)
         return JsonResponse(serializer.data)
-
 
 
 class SetPagination(PageNumberPagination):
@@ -48,4 +47,16 @@ class ProfilePosts(generics.ListCreateAPIView):
                 tagpost.save()
                 return JsonResponse({'status': 'CREATED'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class HomePosts(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = SetPagination
+
+    def get_queryset(self):
+        user = self.request.user.id
+        postUsers = Relation.objects.filter(userFollowing_id=user)
+        print(postUsers)
+        return Post.objects.filter(user__in=[i.followed()for i in postUsers]).order_by('date')
 
