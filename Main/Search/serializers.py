327ddd1ee443099cf21bebc37f5178f2c813986a
Omid Ghahroudi <omid.username.goes.here@gmail.com>
2018-09-03 15:15:51 +0430

    create person serializers for search

--- Main/Search/serializers.py
+++ Main/Search/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from .models import Tag, TagPost
 from Post.serializers import *
-from Account.serializers import *
+from Account.models import *
+
 
 class TagSerializers(serializers.ModelSerializer):
 
@@ -14,3 +15,13 @@ class TagPostSerializers(serializers.ModelSerializer):
     class Meta:
         model = TagPost
         fields = ('tag', 'post')
+
+
+class PersonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Person
+        fields = ('name', 'bio', 'followerNumber',
+                  'followingNumber', 'postNumber',
+                  'phoneNumber', 'username'
+                  )
